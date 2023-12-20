c44705dbdadb53f310d101b6f4880744fcca9383
Theo Crepin <theo.crepin.tab@gmail.com>
2023-12-20 15:22:20 +0100

    free output type API

--- src/converter.py
+++ src/converter.py
@@ -1,20 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+file = "data/elementary/lci/2676.pgm"
+
+
 def read_yuv_pgm(filename):
     """
     Reads a YUV PGM file into a np array.
     """
     with open(filename, "rb") as f:
-        # read the header
         header = f.readline()
         assert header == b"P5\n"
 
-        # read the size
         size = f.readline()
         width, height = [int(x) for x in size.split()]
 
-        # convert the data to a numpy array
         img = plt.imread(filename).astype(np.uint8)
         img = img.reshape((height, width))
 
@@ -49,4 +50,32 @@ def yuv_to_rgb(yuv_img):
 
     return np.stack((r, g, b), axis=-1).astype(np.uint8)
 
+def write_ppm(filename, img):
+    """
+    Writes a PPM file from np array image
+    """
+    with open(filename, "wb") as f:
+        f.write(b"P6\n")
+        f.write(b"%d %d\n" % (img.shape[1], img.shape[0]))
+        f.write(b"255\n")
+        f.write(img.tobytes())
 
+def rgb_from_yuv(filename, output_type="ppm"):
+    """
+    Converts a YUV PGM file to RGB PPM.
+    """
+    img = read_yuv_pgm(filename)
+    img = yuv_to_rgb(img)
+
+    if output_type == "ppm":
+        write_ppm(filename[:-3] + "ppm", img)
+    elif output_type == "display":
+        plt.imshow(img)
+        plt.show()
+    else:
+        raise ValueError("Invalid output type: %s" % output_type) 
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    rgb_from_yuv(file, output_type=args[0] if args else "ppm")
