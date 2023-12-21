cb5c45fab3e86f9c9bdead2a5bba7565aecb21c4
Theo Crepin <theo.crepin.tab@gmail.com>
2023-12-21 17:32:59 +0100

    weave deinterlacer

--- src/deinterlacer.py
+++ src/deinterlacer.py
@@ -5,7 +5,7 @@ import os
 from PIL import Image
 
 
-def deinterlace_bob(input_dir: str, output_dir: str, deinterlace_mask: np.ndarray, top_field_first=True):
+def deinterlace_bob(input_dir: str, output_dir: str, deinterlace_mask: np.ndarray, top_field_first=True) -> None:
     """
     Deinterlaces a PGM sequence using the BOB algorithm.
     """
@@ -19,6 +19,7 @@ def deinterlace_bob(input_dir: str, output_dir: str, deinterlace_mask: np.ndarra
             if top_field_first:
                 lines_top = img[::2]
                 lines_bottom = img[1::2]
+                
             else:
                 lines_top = img[1::2]
                 lines_bottom = img[::2]
@@ -32,3 +33,25 @@ def deinterlace_bob(input_dir: str, output_dir: str, deinterlace_mask: np.ndarra
             count += 1
     
     print(f"Total frames: {count}, from {len(images)} input frames.")
+
+
+def interlace_weave(input_dir: str, output_dir: str) -> None:
+    """
+    Interlaces a PGM sequence using the weave algorithm.
+    """
+    os.makedirs(output_dir, exist_ok=True)
+
+    images = [np.array(Image.open(os.path.join(input_dir, fname))) for fname in sorted(os.listdir(input_dir))]
+
+    count = 0
+    for i in range(0, len(images), 2):
+        if i + 1 < len(images):
+            lines_top = images[i]
+            lines_bottom = images[i + 1]
+
+            interlaced_frame = np.vstack((lines_top, lines_bottom))
+            cv2.imwrite(os.path.join(output_dir, f"{count}.pgm"), interlaced_frame)
+            count += 1
+        else:
+            cv2.imwrite(os.path.join(output_dir, f"{count}.pgm"), images[i])
+            count += 1
